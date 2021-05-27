b2bb6154808e9c14bb4984f9419959c1c7621cc4
HomaGhost <71845224+HomaGhost@users.noreply.github.com>
2021-05-27 20:32:26 +0300

    Add files via upload

---
+++ VPLab 5.3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <string>
+using namespace std;
+
+
+int maxch(int* arr, int size) {
+	int max = arr[0];
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > max) { max = arr[i]; }
+	}
+	return max;
+}
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int* arr, size;
+	cout << "Введите размер: ";
+	cin >> size;
+	arr = new int[size];
+	cout << "Введите числа: " << endl;
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	cout << "Макимальное число: ";
+	cout << maxch(arr, size);
+	delete[] arr;
+}
