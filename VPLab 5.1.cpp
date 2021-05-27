921c1fc48335f0c8dd3ab2b6417b4b22af6306fa
HomaGhost <71845224+HomaGhost@users.noreply.github.com>
2021-05-27 20:26:43 +0300

    Add files via upload

--- VPLab 5.1.cpp
+++ VPLab 5.1.cpp
@@ -1,25 +1,31 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <ctime>
 #include <string>
-
 using namespace std;
+//1 задача
 
-int main()
-{
+int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	string str1, str2;
-	int symbolcount1=0, symbolcount2=0;
-	cout << "Введите первую строку: "<<endl;
-	cin >> str1;
-	cout << "Введите вторую строку: " << endl;
-	cin >> str2;
-	symbolcount1= str1.length();
-	symbolcount2 = str2.length();
-	cout << str1<<endl;
-	cout << str2 << endl;
-	str2.replace(0, symbolcount1, str1, 0, symbolcount2);
-	cout << str2<<endl;
-	system("pause");
+	cout << "Введите первую строку:" << endl;
+	getline(cin, str1);
+	cout << "Введите вторую строку:" << endl;
+	getline(cin, str2);
+	if (str1 == str2) {
+		cout << "Строки одинаковые";
+		exit(1);
+	}
+
+	for (int i = 0; i < str1.size(); i++) {
+		if (str1[i] == str2[i]) {
+			continue;
+		}
+		cout << "Количество одинаковых первых символов: " << i;
+		break;
+	}
+
+
 	return 0;
 }
---
+++ VPLab 5.2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <string>
+using namespace std;
+
+
+int main() {
+	string predlojenie;
+	int fprob = 0, lprob = 0;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите предложение:" << endl;
+	getline(cin, predlojenie);
+
+	for (int i = 0; i < predlojenie.size(); i++) {
+		if (predlojenie[i] == ' ') {
+			fprob = i;
+			break;
+		}
+	}
+	//первый пробел
+
+	for (int i = predlojenie.size(); i > 0; i--) {
+		if (predlojenie[i] == ' ') {
+			lprob = i;
+			break;
+		}
+	}
+	//последний пробел
+	if (lprob == 0) {
+		cout << "Введено только одно слово";
+		exit(1);
+	}
+
+	for (int i = lprob + 1; i < predlojenie.size(); i++) {
+		cout << predlojenie[i];
+	}
+	for (int i = fprob; i < lprob + 1; i++) {
+		cout << predlojenie[i];
+	}
+	for (int i = 0; i < fprob; i++) {
+		cout << predlojenie[i];
+	}
+	return 0;
+}
