3ceec0c476415044a3781ceafb4e698e20396bdb
HomaGhost <71845224+HomaGhost@users.noreply.github.com>
2021-06-27 23:36:39 +0300

    Add files via upload

---
+++ VPLab 8.3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int kolvostrok = 0, numstr=0;
+	bool flag = 0;
+
+	ifstream fin("info3.txt");
+
+	if (!fin)
+	{
+		cerr << "File could not be opened for reading!" << endl;
+		exit(1);
+	}
+
+	while (fin)
+	{
+		string str;
+		getline(fin, str);
+		cout << str << endl;
+		kolvostrok++;
+		if (str[0] == 'T' && flag == 0) {
+			numstr = kolvostrok;
+			flag = 1;
+		}
+	}
+	cout << "Первый раз элемент Т в " << numstr << "-ой строке";
+
+	return 0;
+}
